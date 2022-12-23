6932fef083543398bd05786db4ebd10bd6c372c9
ehsan-afsharnia <iamafsharnia@gmail.com>
2022-12-23 12:24:16 +0330

    initialization

---
+++ js/app.js
@@ -0,0 +1,37 @@
+// variables
+
+const sendBtn = document.querySelector('#sendBtn')
+
+
+
+
+
+
+
+// eventlisteners
+
+eventlisteners()
+
+
+
+function eventlisteners(){
+
+// app initialization
+
+document.addEventListener('DOMContentLoaded', appInit)
+
+    
+}
+
+
+
+
+
+// functions 
+
+function appInit(){
+
+
+sendBtn.disabled = true
+
+}
